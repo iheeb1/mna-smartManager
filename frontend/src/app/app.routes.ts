397c43177c35e0762e06ab1dd3509e4696d551da
benromdhaneiheb2@gmail.com <benromdhaneiheb2@gmail.com>
2025-09-25 01:06:50 +0100

    project initial setup with database connection test

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'users',
+    loadComponent: () => import('./components/users/users.component').then(m => m.UsersComponent)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./components/settings/settings.component').then(m => m.SettingsComponent)
+  },
+  {
+    path: 'reports',
+    loadComponent: () => import('./components/reports/reports.component').then(m => m.ReportsComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
