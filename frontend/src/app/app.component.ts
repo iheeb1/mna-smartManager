dfc7335c102ad90ac79d4683491281abab48c099
benromdhaneiheb2@gmail.com <benromdhaneiheb2@gmail.com>
2025-09-28 17:14:21 +0100

    initial login page setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [LoginComponent],
+  template: `<app-login></app-login>`,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'frontend';
---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,102 @@
+// src/app/components/login/login.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// PrimeNG Modules (v17.18.0 - NgModule-based)
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { DialogModule } from 'primeng/dialog';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    DialogModule,
+    RippleModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  // Dialog visibility flags
+  passwordDialogVisible = false;
+  textDialogVisible = false;
+
+  // Mobile form data
+  mobileUsername = '';
+  mobilePassword = '';
+  mobileId = '';
+  mobilePhoneNumber = '0509100726';
+
+  // Desktop form data
+  desktopPassword = '';
+  desktopId = '12345678';
+  desktopTextId = '0509100726';
+  desktopTextCode = '';
+
+  // Tab states for mobile cards
+  mobileActiveTab1 = 'password'; // 'password' or 'sms'
+  mobileActiveTab2 = 'sms';      // 'sms' or 'password'
+
+  // Dialog control methods
+  showPasswordDialog() {
+    this.passwordDialogVisible = true;
+  }
+
+  hidePasswordDialog() {
+    this.passwordDialogVisible = false;
+  }
+
+  onPasswordDialogHide() {
+    this.passwordDialogVisible = false;
+  }
+
+  showTextDialog() {
+    this.textDialogVisible = true;
+  }
+
+  hideTextDialog() {
+    this.textDialogVisible = false;
+  }
+
+  onTextDialogHide() {
+    this.textDialogVisible = false;
+  }
+
+  // Login handlers
+  onMobileLogin() {
+    console.log('Mobile login:', {
+      username: this.mobileUsername,
+      password: this.mobilePassword,
+      id: this.mobileId
+    });
+  }
+
+  onDesktopPasswordLogin() {
+    console.log('Desktop password login:', {
+      password: this.desktopPassword,
+      id: this.desktopId
+    });
+    this.hidePasswordDialog();
+  }
+
+  onDesktopTextLogin() {
+    console.log('Desktop text login:', {
+      code: this.desktopTextCode,
+      id: this.desktopTextId
+    });
+    this.hideTextDialog();
+  }
+
+  sendTextCode() {
+    console.log('Sending text code to:', this.mobilePhoneNumber);
+  }
+}
